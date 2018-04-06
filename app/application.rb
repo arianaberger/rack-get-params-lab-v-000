745ddf2bcf6962248da64fdc869b96ea47280de7
Ariana Berger <ariana.berger@gmail.com>
2018-04-06 17:08:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -42,7 +42,7 @@ class Application
       if @@items.include?(search_term)
 
         @@cart << search_term
-        resp.write "added #{item}"
+        resp.write "added #{search_term}"
       else
         resp.write "We don't have that item!"
       end
